36aac362a34f9257961a6cd2b155f72849083c97
Max Kravchenko <maks.cross@gmail.com>
2023-01-20 18:29:43 +0200

    add .env

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-//N1ESCCAEDLT03ZjJ
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const { DB_HOST } = require('./config');
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+console.log(DB_HOST);
 
 mongoose
   .connect(DB_HOST)
